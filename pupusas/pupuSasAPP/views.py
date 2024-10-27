ee02d67a1ffc53a0d846f120266561a6160c33ac
Tu Nombre <tuemail@example.com>
2024-10-26 19:49:25 -0600

    yo

--- pupusas/pupuSasAPP/views.py
+++ pupusas/pupuSasAPP/views.py
@@ -3,8 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Pupuseria juan Perez')
+#def index(request):
+    #return HttpResponse('Pupuseria juan Perez')
 
 def home(requets):
     return render(requets,'inicio.html')
+
+def pagos(request):
+    return render(request, 'pagos.html')
+
+def reportes(request):
+    return render(request,'reportes.html' )
+
+def inven(request):
+    return render(request, 'inventario.html')
