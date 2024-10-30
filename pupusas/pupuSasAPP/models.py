a72877c530f5de04c353cae31dda2884a271a84a
Carlos-Larin <nekokar701@gmail.com>
2024-10-30 17:02:06 -0600

    pagos update

--- pupusas/pupuSasAPP/models.py
+++ pupusas/pupuSasAPP/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class pagoPupas(models.Model):
+    cantidadPupusas= models.IntegerField()
+    cantidadBebida= models.IntegerField()
+    correoCliente= models.EmailField(max_length=200)
+    pagaCon = models.DecimalField(max_digits=10, decimal_places=4)
